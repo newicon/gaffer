057f789b61531ae5d516916ee861bf31d24add90
Dougall Winship <dougall.winship@newicon.net>
2023-10-11 12:15:00 +0100

    readme tweaks added the missing cli.php

--- README.md
+++ README.md
@@ -9,5 +9,20 @@ a minimal [PSR](https://www.php-fig.org/psr/)/[composer](https://getcomposer.org
 ## installation
 - clone this repo (duh!)
 - setup hosts/vhosts (as required) pointing to /public
-- browse away
-- to view the admin area you will need to create a user via cli.php
+- create a /config.ini file like this, something like this
+```
+ENV = 'development'
+DEBUG = 'true'
+TIMEZONE = 'Europe/London'
+EMAIL = 'gaffer@newicon.net'
+
+DB_HOST = 'localhost'
+DB_NAME = 'gaffer_dev'
+DB_USER = 'root'
+DB_PASS = 'root'
+DB_CHAR = 'utf8'
+```
+- create a local database using the scripts in /sql 
+- browse away!
+- to view the admin area you will need to create an admin user via cli.php
+```php cli.php add-user {email} {password}```
---
+++ cli.php
@@ -0,0 +1,56 @@
+<?php
+
+use App\Model\User;
+
+include __DIR__.'/vendor/autoload.php';
+
+$scriptName = $argv[0];
+$command = $argv[1] ?? null;
+$params = array_slice($argv,2);
+
+// this is really bodged ... there should be a dedicated CLI bootstrap!
+$ini = parse_ini_file(__DIR__.DIRECTORY_SEPARATOR."/config.ini");
+$connectionString = "mysql:dbname=".$ini['DB_NAME'].";host=".$ini['DB_HOST'].";charset=".$ini['DB_CHAR'];
+\System\db\DB::init($connectionString, $ini['DB_USER'], $ini['DB_PASS']);
+
+switch ($command)
+{
+    case 'add-user':
+        if (count($params)<2) {
+            die("Missing required parameters");
+        }
+        $email = $params[0];
+        $password = $params[1];
+        $user = User::getUserByEmail($email);
+        if ($user) {
+            die("User with email '".$email."' already exists");
+        }
+        $user = new User();
+        $user->email = $email;
+        $user->setPassword($password);
+        $user->status='active';
+        $user->save();
+        echo "Added user '".$email."'".PHP_EOL;
+        break;
+
+    case 'set-password':
+        if (count($params)<2) {
+            die("Missing required parameters");
+        }
+        $email = $params[0];
+        $password = $params[1];
+        $user = User::getUserByEmail($email);
+        if (!$user) {
+            die("User with email '".$email."' doesn't exist");
+        }
+        $user->setPassword($password);
+        $user->save();
+        echo "Updated password for user '".$email."'".PHP_EOL;
+        break;
+
+    default:
+        echo "Commands:".PHP_EOL;
+        echo "  add-user email password".PHP_EOL;
+        echo "  set-password email password".PHP_EOL;
+        break;
+}
